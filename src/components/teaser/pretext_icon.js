b08bb19ec5de19052ee2b7f53716cdc47573259e
Emmanuel Manarang <emmanuel_manarang_ensemble@whirlpool.com>
2021-03-31 12:06:51 -0700

    initial changes to teasers. changed in home page

---
+++ src/components/teaser/pretext_icon.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+  const x = document.getElementsByClassName('teaser');
+  const l = x.length;
+
+  for (let i = 0; i < l; i++) {
+    console.log(i, x.length);
+
+    if (x[i].classList.contains("homeHeader")) {
+      const y = x[i].getElementsByClassName("cmp-teaser__pretitle");
+      if (y.length) {
+        const inner = y[0].innerHTML.toLocaleLowerCase();
+        if (inner.startsWith("[[icon=") && inner.endsWith("]]") && inner.length > 9) {
+          let path = inner.substr(7, inner.length - 9);
+          y[0].innerHTML = `<img src="${path}" alt="${path}" />`
+        }
+      }
+    } else if (x[i].classList.contains("boxedRightText")) {
+      const y = x[i].getElementsByTagName("img");
+      console.log(y.length, y);
+    }
+  }
+})
