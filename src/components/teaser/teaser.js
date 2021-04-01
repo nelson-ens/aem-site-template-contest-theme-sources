f876dbc00a522a896bc2f2394244168a1918d23e
Emmanuel Manarang <emmanuel_manarang_ensemble@whirlpool.com>
2021-04-01 09:47:21 -0700

    Removed commented codes and lines used for testing.

--- src/components/teaser/teaser.js
+++ src/components/teaser/teaser.js
@@ -3,21 +3,16 @@ document.addEventListener('DOMContentLoaded', function (event) {
   const l = x.length;
 
   for (let i = 0; i < l; i++) {
-    console.log(i, x.length);
-
     if (x[i].classList.contains("homeHeader") || x[i].classList.contains("contentHeader")) {
       const y = x[i].getElementsByClassName("cmp-teaser__pretitle");
       if (y.length) {
         const inner = y[0].innerHTML.toLocaleLowerCase();
+        // This will check if the pretext want's an icon. Uploaded image must be named with lowercase.
         if (inner.startsWith("[[icon=") && inner.endsWith("]]") && inner.length > 9) {
           let path = inner.substr(7, inner.length - 9);
           y[0].innerHTML = `<img class="cmp-teaser__pretitle-icon" src="${path}" alt="${path}" />`
         }
       }
     } 
-    // else if (x[i].classList.contains("boxedRightText")) {
-    //   const y = x[i].getElementsByTagName("img");
-    //   console.log(y.length, y);
-    // }
   }
 })
