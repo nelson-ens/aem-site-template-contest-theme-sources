ae8c15a19790a5a678737d8c2eea27d83a02179c
Emmanuel Manarang <emmanuel_manarang_ensemble@whirlpool.com>
2021-03-31 20:23:19 -0700

    done with the teasers. Just fine tuning left if needed.

--- src/components/teaser/pretext_icon.js
+++ src/components/teaser/pretext_icon.js
@@ -5,18 +5,19 @@ document.addEventListener('DOMContentLoaded', function (event) {
   for (let i = 0; i < l; i++) {
     console.log(i, x.length);
 
-    if (x[i].classList.contains("homeHeader")) {
+    if (x[i].classList.contains("homeHeader") || x[i].classList.contains("contentHeader")) {
       const y = x[i].getElementsByClassName("cmp-teaser__pretitle");
       if (y.length) {
         const inner = y[0].innerHTML.toLocaleLowerCase();
         if (inner.startsWith("[[icon=") && inner.endsWith("]]") && inner.length > 9) {
           let path = inner.substr(7, inner.length - 9);
-          y[0].innerHTML = `<img src="${path}" alt="${path}" />`
+          y[0].innerHTML = `<img class="cmp-teaser__pretitle-icon" src="${path}" alt="${path}" />`
         }
       }
-    } else if (x[i].classList.contains("boxedRightText")) {
-      const y = x[i].getElementsByTagName("img");
-      console.log(y.length, y);
-    }
+    } 
+    // else if (x[i].classList.contains("boxedRightText")) {
+    //   const y = x[i].getElementsByTagName("img");
+    //   console.log(y.length, y);
+    // }
   }
 })
