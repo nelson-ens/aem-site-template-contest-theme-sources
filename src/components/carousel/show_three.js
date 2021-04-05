bd40b0fc916d76e2b6ee1f89922c111bab94b19a
Amy Shao <amys@ensemble.com>
2021-04-05 13:15:25 -0700

    redo three image carousel using hide/show instead of inserting, add image border radius

--- src/components/carousel/show_three.js
+++ src/components/carousel/show_three.js
@@ -1,68 +1,71 @@
 class CarouselWithThreeImages {
   constructor(carousel) {
+    this.content;
     this.numOfImages;
-    this.left;
-    this.right;
     this.allImages;
-    this.insertImages(carousel);
+    this.nextButton;
+    this.prevButton;
+    this.initCarousel(carousel);
   }
 
-  getImageData(index) {
-    return this.allImages[index].getElementsByClassName("cmp-image")[0].dataset;
+  showImage(index) {
+    var img = this.allImages[index];
+    if (img == null) return;
+    img.style.display = 'flex';
   }
 
-  createInsertedImage(imgURL, text) {
-    var container = document.createElement("div");
-    var image = document.createElement("img");
-    var caption = document.createElement("span");
-    image.src = imgURL;
-    caption.textContent = text;
-    container.setAttribute("class", "inserted-image");
-    container.appendChild(image);
-    container.appendChild(caption);
-    return container;
+  hideImage(index) {
+    var img = this.allImages[index];
+    if (img == null) return;    
+    img.style.display = 'none';
   }
 
-  insertImages(carousel) {
-    if (!carousel) return;
-    var content = carousel.getElementsByClassName("cmp-carousel__content")[0];
-    var images = content.getElementsByClassName("cmp-carousel__item");
-    this.numOfImages = images.length;
-    this.left = this.numOfImages - 1;
-    this.right = 1;
-    this.allImages = images;
-
-    var leftData = this.getImageData(this.left);
-    var rightData = this.getImageData(this.right);
-
-    var leftImage = this.createInsertedImage(leftData.asset, leftData.title);
-    var rightImage = this.createInsertedImage(rightData.asset, rightData.title);
-
-    content.insertBefore(leftImage, content.firstChild);
-    content.insertBefore(rightImage, content.lastChild);
-  }
-
-  updateImage(position, newIndex) {
-    var imgPosition = position === "left" ? 0 : 1;
-    var insertedImages = document.getElementsByClassName("inserted-image");
-    var image = insertedImages[imgPosition].getElementsByTagName("img")[0];
-    var caption = insertedImages[imgPosition].getElementsByTagName("span")[0];
-    var data = this.getImageData(newIndex);
-
-    image.src = data.asset;
-    caption.textContent = data.title;
-  }
-
-  moveImages(direction) {
-    if (direction === "next") {
-      this.left = this.left + 1 >= this.numOfImages ? 0 : this.left + 1;
-      this.right = this.right + 1 >= this.numOfImages ? 0 : this.right + 1;
-    } else {
-      this.left = this.left - 1 < 0 ? this.numOfImages - 1 : this.left - 1;
-      this.right = this.right - 1 < 0 ? this.numOfImages - 1 : this.right - 1;
+  hideImages() {
+    for (var i = 0; i < 3; i++) {
+      this.hideImage(i);
     }
-    this.updateImage("left", this.left);
-    this.updateImage("right", this.right);
+  }
+  
+  showImages() {
+    for (var i = 0; i < 3; i++) {
+      this.showImage(i);
+    }
+  }
+  
+  moveToEnd() {
+    var firstImage = this.allImages[0];
+    this.content.appendChild(firstImage, this.content.lastChild);
+  }
+
+  moveToFront() {
+    var lastImage = this.allImages[this.numOfImages-1];
+    this.content.insertBefore(lastImage, this.content.firstChild);
+  }
+
+  initCarousel(carousel) {
+    if (!carousel) return;
+    this.content = carousel.getElementsByClassName("cmp-carousel__content")[0];
+    this.allImages = this.content.getElementsByClassName("cmp-carousel__item");
+    this.numOfImages = this.allImages.length;
+    
+    var buttons = carousel.getElementsByClassName("cmp-carousel__action");
+    buttons[0].onclick = () => this.moveImages('prev');
+    buttons[1].onclick = () => this.moveImages('next');
+    this.prevButton = buttons[0];
+    this.nextButton = buttons[1];
+    
+    this.showImages();
+  }
+
+  
+  moveImages(direction) {
+    this.hideImages();
+    if (direction === "next") {
+      this.moveToEnd();
+    } else {
+      this.moveToFront();
+    }
+    this.showImages();
   }
 }
 
@@ -75,20 +78,6 @@ function initThreeImageCarousels() {
   }
 }
 
-window.addEventListener("click", function (event) {
-  for (var i = 0; i < allCarousels.length; i++) {
-    carousel = allCarousels[i];
-    if (!carousel) return;
-    var buttons = document.getElementsByClassName("cmp-carousel__action");
-    var prev = buttons[0];
-    var next = buttons[1];
-
-    if (prev.contains(event.target)) {
-      carousel.moveImages("prev");
-    } else if (next.contains(event.target)) {
-      carousel.moveImages("next");
-    } else return;
-  }
+window.addEventListener('DOMContentLoaded', (event) => {
+  initThreeImageCarousels();
 });
-
-window.onload = initThreeImageCarousels;
